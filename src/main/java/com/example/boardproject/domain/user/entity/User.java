1fe440daea5f69e238473b9d341067c983f27d26
LeeYulhee <reasonoflife39@gmail.com>
2026-01-28 13:36:14 +0900

    feat : User, Post Entity 추가

---
+++ src/main/java/com/example/boardproject/domain/user/entity/User.java
@@ -0,0 +1,52 @@
+package com.example.boardproject.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.sql.Types;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "user")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @UuidGenerator(style = UuidGenerator.Style.RANDOM)
+    @JdbcTypeCode(Types.VARCHAR)
+    @Column(name = "user_id", columnDefinition = "VARCHAR(36)")
+    private UUID userId;
+
+    @Column(name = "login_id", length = 50, unique = true, nullable = false)
+    private String loginId;
+
+    @Column(name = "password", length = 255, nullable = false)
+    private String password;
+
+    @Column(name = "nickname", length = 50, nullable = false)
+    private String nickname;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public User(String loginId, String password, String nickname) {
+        this.loginId = loginId;
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
